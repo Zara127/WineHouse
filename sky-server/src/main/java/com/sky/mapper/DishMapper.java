45cfd51088209ef39d5e604738e1783a880c5e81
zz <1335986373@qq.com>
2025-04-23 12:51:03 +0800

    套餐管理业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -56,4 +56,20 @@ public interface DishMapper {
     @AutoFill(value = OperationType.UPDATE)//自动填充公共字段
     //使用动态sql，因为不确定更新哪些属性
     void update(Dish dish);
+
+    /**
+     * 根据条件查询菜品数据
+     * @param dish
+     * @return
+     */
+    List<Dish> list(Dish dish);
+
+
+    /**
+     * 根据套餐id查询菜品
+     * @param setmealId
+     * @return
+     */
+    @Select("select a.* from dish a left join setmeal_dish b on a.id = b.dish_id where b.setmeal_id = #{setmealId}")
+    List<Dish> getBySetmealId(Long setmealId);
 }
