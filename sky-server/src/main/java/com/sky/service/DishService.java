c157ecac9153b3ae84b9cd0363f88cc207b65092
zz <1335986373@qq.com>
2025-04-23 10:15:30 +0800

    修改酒品业务代码开发

---
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -0,0 +1,44 @@
+package com.sky.service;
+
+import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.vo.DishVO;
+
+import java.util.List;
+
+public interface DishService {
+    /**
+     * 新增菜品和口味
+     * @param dishDTO
+     */
+    void saveWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 删除菜品
+     * @param ids
+     * @return
+     */
+    void deleteBatch(List<Long> ids);
+
+    /**
+     * 根据id查询菜品和口味
+     * @param id
+     * @return
+     */
+    DishVO getByIdWithFlavor(Long id);
+
+    /**
+     * 修改菜品和口味信息
+     * @param dishDTO
+     * @return
+     */
+    void updateWithFlavor(DishDTO dishDTO);
+}
